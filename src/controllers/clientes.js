8d5a8a0b1e5c9db35138e89911008ea5c76bb446
Gabriel Nogueira <gnogueira27@gmail.com>
2020-11-25 23:31:16 -0300

    feat(controllers/clientes): added new function new function added to update clients data in database

--- src/controllers/clientes.js
+++ src/controllers/clientes.js
@@ -28,4 +28,40 @@ const addNewClient = async (ctx) => {
 	}
 };
 
-module.exports = { addNewClient };
+const updateClientData = async (ctx) => {
+	const clientDataToBeUpdated = ctx.request.body;
+	const formattedCpf = formatter.cpfFormatter(clientDataToBeUpdated.cpf);
+
+	const checkIfClientExists = await clientFunctionsQueries.checkIfClientAlreadyExists(
+		formattedCpf
+	);
+
+	console.log(checkIfClientExists);
+
+	if (clientDataToBeUpdated && !checkIfClientExists) {
+		const updateClientDataQueryResult = await clientFunctionsQueries.queryToUpdateClientData(
+			clientDataToBeUpdated.id,
+			clientDataToBeUpdated.nome,
+			formattedCpf,
+			clientDataToBeUpdated.email,
+			clientDataToBeUpdated.tel
+		);
+
+		console.log(updateClientDataQueryResult);
+
+		return response(ctx, 200, {
+			dados: {
+				id: updateClientDataQueryResult.id,
+				nome: updateClientDataQueryResult.name,
+				cpf: updateClientDataQueryResult.cpf,
+				email: updateClientDataQueryResult.email,
+				tel: updateClientDataQueryResult.phone,
+			},
+		});
+	}
+	if (clientDataToBeUpdated && checkIfClientExists) {
+		return response(ctx, 404, 'Esse CPF já foi usado em outro cadastro.');
+	}
+};
+
+module.exports = { addNewClient, updateClientData };
