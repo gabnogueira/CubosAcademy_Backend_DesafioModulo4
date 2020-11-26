ddc334c63a8139d3e6ecdbe385144a8d59e494a1
Gabriel Nogueira <gnogueira27@gmail.com>
2020-11-25 23:30:16 -0300

    feat(routes): add new endpoint added new endpoint to update client data function

--- src/routes.js
+++ src/routes.js
@@ -9,9 +9,14 @@ const router = new Router();
 //* * Endpoints de login e usuário */
 router.post('/auth', autenticacao.autenticarUser);
 router.post('/usuarios', funcoesDeUsuario.adicionarUsuario);
-router.post('/clientes', verifyUser.verificacao, clientFuncions.addNewClient);
 
 //* * Endpoints de clientes */
+router.post('/clientes', verifyUser.verificacao, clientFuncions.addNewClient);
+router.put(
+	'/clientes',
+	verifyUser.verificacao,
+	clientFuncions.updateClientData
+);
 
 //* * Endpoints de cobranças */
 
