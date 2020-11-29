aecaad66257110d189f626ccf9c4ade382be6f78
Gabriel Nogueira <gnogueira27@gmail.com>
2020-11-29 20:53:37 -0300

    fix (src/routes): changed variables & routes names changed variables and routes names from PT-BR to EN to keep project language standard

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,16 @@
 const Router = require('koa-router');
-const autenticacao = require('./controllers/autenticacao');
-const funcoesDeUsuario = require('./controllers/usuarios');
+const authentication = require('./controllers/authentication');
+const userFunctions = require('./controllers/users');
 const verifyUser = require('./middlewares/session');
-const clientFunctions = require('./controllers/clientes');
-const chargeFunctions = require('./controllers/cobrancas');
+const clientFunctions = require('./controllers/clients');
+const chargeFunctions = require('./controllers/charges');
 // const { verificacao } = require('./middlewares/session');
 
 const router = new Router();
 
 //* * Endpoints de login e usuário */
-router.post('/auth', autenticacao.autenticarUser);
-router.post('/usuarios', funcoesDeUsuario.adicionarUsuario);
+router.post('/auth', authentication.authUser);
+router.post('/usuarios', userFunctions.addNewUser);
 
 //* * Endpoints de clientes */
 router.post('/clientes', verifyUser.verificacao, clientFunctions.addNewClient);
