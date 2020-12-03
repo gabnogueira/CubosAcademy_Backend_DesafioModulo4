eb120f753581fec04d70d24c9b9cb1655da3b248
Gabriel Nogueira <gnogueira27@gmail.com>
2020-12-02 23:30:50 -0300

    feat (src/routes): added new route and minor fix added new route to list clients and search clients endpoints. also changed name of verifyUser variable to verifyFunction

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router');
 const authentication = require('./controllers/authentication');
 const userFunctions = require('./controllers/users');
-const verifyUser = require('./middlewares/session');
+const verifyFunction = require('./middlewares/session');
 const clientFunctions = require('./controllers/clients');
 const chargeFunctions = require('./controllers/charges');
 // const { verificacao } = require('./middlewares/session');
@@ -13,20 +13,34 @@ router.post('/auth', authentication.authUser);
 router.post('/usuarios', userFunctions.addNewUser);
 
 //* * Endpoints de clientes */
-router.post('/clientes', verifyUser.verificacao, clientFunctions.addNewClient);
+router.post(
+	'/clientes',
+	verifyFunction.verificacao,
+	clientFunctions.addNewClient
+);
 router.put(
 	'/clientes',
-	verifyUser.verificacao,
+	verifyFunction.verificacao,
 	clientFunctions.updateClientData
+);
+
+router.get(
+	'/clientes',
+	verifyFunction.verificacao,
+	clientFunctions.listOrSearchClients
 );
 
 //* * Endpoints de cobranças */
 router.post(
 	'/cobrancas',
-	verifyUser.verificacao,
+	verifyFunction.verificacao,
 	chargeFunctions.createCharges
 );
-router.get('/cobrancas', verifyUser.verificacao, chargeFunctions.listCharges);
-router.put('/cobrancas', verifyUser.verificacao, chargeFunctions.payCharge);
+router.get(
+	'/cobrancas',
+	verifyFunction.verificacao,
+	chargeFunctions.listCharges
+);
+router.put('/cobrancas', verifyFunction.verificacao, chargeFunctions.payCharge);
 
 module.exports = router;
